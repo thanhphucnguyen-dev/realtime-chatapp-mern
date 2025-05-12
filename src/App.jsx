ed7368b33034c151605f5d515b23b320e72fdefd
Nguyen Thanh Phuc <thanhphucnguyen54@gmail.com>
2025-05-12 21:41:26 +0700

    Add: General App UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
 import DashboardLayout from './layouts/dashboard'
+import GeneralApp from './pages/dashboard/GeneralApp'
+import { Box } from '@mui/material'
 
 function App() {
 
   return (
-    <>
-      <DashboardLayout />
-    </>
+    <Box sx={{ display: 'flex' }}>
+      <Box>
+        <DashboardLayout />
+      </Box>
+      <Box>
+        <GeneralApp />
+      </Box>
+    </Box>
+
   )
 }
 
